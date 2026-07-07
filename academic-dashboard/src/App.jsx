b1be24e3f8a4fca6ea9b3e6e190f573d8b6a6bd9
harivignesh_ravindrakumar <harivignesh@ooivuindia.in>
2026-07-07 15:37:30 +0530

    firebase auth connect to login page

--- academic-dashboard/src/App.jsx
+++ academic-dashboard/src/App.jsx
@@ -1,11 +1,16 @@
-import LoginPage from "./components/LoginPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return(
-    <>
-    <LoginPage />
-    </>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ academic-dashboard/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-[#B57A2B]">
+        Dashboard
+      </h1>
+    </div>
+  );
+}
